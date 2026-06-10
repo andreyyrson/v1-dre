996d14336433f02bd7c7b7c99a179ee0173c9aca
andreyyrson <andreyrson2000@gmail.com>
2026-06-10 08:53:42 -0300

    fix: install setuptools before editable install

--- dextro-BI/start.sh
+++ dextro-BI/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pip install setuptools
 cd backend
-pip install -e . > /dev/null 2>&1
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
+pip install -e .
+python -m uvicorn app.main:app --host 0.0.0.0 --port $PORT
